ed82b703855b14323a0a33fbf3548c74e2f7e33f
Mohit9359 <mohit.mb.2234@gmail.com>
2025-09-13 22:10:30 +0530

    yo

---
+++ leetcode/duplicates.java
@@ -0,0 +1,31 @@
+package leetcode;
+public class duplicates{
+    public static void duplicate(int nums[]){
+        int count =1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=nums[count-1]){
+                nums[count]=nums[i];
+                count++;
+            }
+
+
+            
+        }
+
+        for(int i=0;i<count;i++){
+            System.out.println(nums[i]);
+        }
+        System.out.println(count);
+
+    }
+    public static void main(String[] args) {
+        int nums[]={1,1,2,2,2,3,4,4,4,4,5};
+        duplicate(nums);
+    }
+
+
+
+
+
+}
+
